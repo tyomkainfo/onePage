5abecec220b17654d53e33e18ea89b48e9883a0e
kaleriiaoh <kaleriya.ovanlig@gmail.com>
2020-12-13 16:13:50 +0200

    kaleriia, section "skills"

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+$( function() {
+    $( ".design" ).progressbar({
+        value: 75
+    });
+    $( ".development" ).progressbar({
+        value: 90
+    });
+    $( ".marketing" ).progressbar({
+        value: 65
+    });
+} );
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
